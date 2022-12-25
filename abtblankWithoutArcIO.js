f7394f7b2668b6ff9d58ba153ff1c03174010fa6
Radioactive Potato <75719839+RadioactivePotato@users.noreply.github.com>
2022-12-25 23:18:48 +0000

    Update abtblankWithoutArcIO.js

--- abtblankWithoutArcIO.js
+++ abtblankWithoutArcIO.js
@@ -1,5 +1,5 @@
 // Hello there!
-//
+// code from: https://3kh0.github.io/js/main.js
 // If you want to add my games to your site, please reach out at my email: echo-the-coder@tuta.io, or discord: 3kh0_#1791
 
 console.warn(
